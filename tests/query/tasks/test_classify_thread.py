80059119f212b816250b6eeef150bf6979b12900
ayush-iitkgp <ayushpandey.iitkgp@gmail.com>
2024-06-05 22:29:25 +0200

    Add github action for test

--- tests/query/tasks/test_classify_thread.py
+++ tests/query/tasks/test_classify_thread.py
@@ -8,9 +8,6 @@ pytestmark = pytest.mark.django_db
 def test_classify_thread(question_answer) -> None:
     thread = question_answer.thread
     assert thread.label is None
-    classify_thread.apply(
-        throw=True, kwargs={"question_answer_id": str(question_answer.id)}
-    )
-    question_answer.refresh_from_db()
-    thread = question_answer.thread
+    classify_thread.apply(throw=True, kwargs={"thread_id": str(thread.id)})
+    thread.refresh_from_db()
     assert thread.label is not None
